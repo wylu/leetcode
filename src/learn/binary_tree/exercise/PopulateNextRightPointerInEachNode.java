0738ec7eacaeb1e53b6f062e28d7d876b7193a7e
wylu <158677478@qq.com>
2020-04-19 23:32:54 +0800

    some binary tree exercise

--- src/learn/binary_tree/exercise/PopulateNextRightPointerInEachNode.java
+++ src/learn/binary_tree/exercise/PopulateNextRightPointerInEachNode.java
@@ -30,6 +30,24 @@ public class PopulateNextRightPointerInEachNode {
         return root;
     }
 
+    public Node connectRecursive(Node root) {
+        if (root == null) {
+            return null;
+        }
+        helper(root.left, root.right);
+        return root;
+    }
+
+    private void helper(Node node1, Node node2) {
+        if (node1 == null) {
+            return;
+        }
+        node1.next = node2;
+        helper(node1.left, node1.right);
+        helper(node2.left, node2.right);
+        helper(node1.right, node2.left);
+    }
+
     public static void prt(Node root) {
         if (root == null) {
             return;
@@ -43,9 +61,10 @@ public class PopulateNextRightPointerInEachNode {
                 sb.append(cur.val).append(',');
                 cur = cur.next;
             }
-            sb.append('#');
+            sb.append('#').append(',');
             levelStart = levelStart.left;
         }
+        sb.deleteCharAt(sb.length() - 1);
         sb.append(']');
         System.out.println(sb.toString());
     }
@@ -56,6 +75,10 @@ public class PopulateNextRightPointerInEachNode {
         Node tree = Node.mkTreeFromPreAndIn(pre, in);
         PopulateNextRightPointerInEachNode populate = new PopulateNextRightPointerInEachNode();
         populate.connect(tree);
+        PopulateNextRightPointerInEachNode.prt(tree);
+
+        tree = Node.mkTreeFromPreAndIn(pre, in);
+        populate.connectRecursive(tree);
         PopulateNextRightPointerInEachNode.prt(tree);
     }
 }
