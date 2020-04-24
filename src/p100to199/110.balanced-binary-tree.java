6ccd098b6425b3fbea77e588afc647e78ee9cff2
wylu <158677478@qq.com>
2020-04-24 21:03:34 +0800

    Tree DFS

---
+++ src/p100to199/110.balanced-binary-tree.java
@@ -0,0 +1,103 @@
+package p100to199;
+
+import common.TreeNode;
+
+/*
+ * @lc app=leetcode id=110 lang=java
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (42.83%)
+ * Likes:    1937
+ * Dislikes: 151
+ * Total Accepted:    419.1K
+ * Total Submissions: 978.4K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ * 
+ * For this problem, a height-balanced binary tree is defined as:
+ * 
+ * 
+ * a binary tree in which the left and right subtrees of every node differ in
+ * height by no more than 1.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Given the following tree [3,9,20,null,null,15,7]:
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * Return true.
+ * 
+ * Example 2:
+ * 
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ * 
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ * 
+ * 
+ * Return false.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * @File    :   110.balanced-binary-tree.java
+ * @Time    :   2020/04/24 20:53:24
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+class Solution110 {
+    public boolean isBalanced(TreeNode root) {
+        return balance(root) != -1;
+    }
+
+    private int balance(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = balance(root.left);
+        int right = balance(root.right);
+        if (left == -1 || right == -1) {
+            return -1;
+        }
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(left, right);
+    }
+}
+// @lc code=end
+
