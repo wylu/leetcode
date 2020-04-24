8a229c48de889999d4c46b72830780bd028cc586
wylu <158677478@qq.com>
2020-04-24 20:51:37 +0800

    Tree DFS

---
+++ src/p100to199/108.convert-sorted-array-to-binary-search-tree.java
@@ -0,0 +1,87 @@
+package p100to199;
+
+import common.TreeNode;
+
+/*
+ * @lc app=leetcode id=108 lang=java
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ *
+ * https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (56.05%)
+ * Likes:    2015
+ * Dislikes: 190
+ * Total Accepted:    371.1K
+ * Total Submissions: 661.8K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * Given an array where elements are sorted in ascending order, convert it to a
+ * height balanced BST.
+ * 
+ * For this problem, a height-balanced binary tree is defined as a binary tree
+ * in which the depth of the two subtrees of every node never differ by more
+ * than 1.
+ * 
+ * Example:
+ * 
+ * 
+ * Given the sorted array: [-10,-3,0,5,9],
+ * 
+ * One possible answer is: [0,-3,9,-10,null,5], which represents the following
+ * height balanced BST:
+ * 
+ * ⁠     0
+ * ⁠    / \
+ * ⁠  -3   9
+ * ⁠  /   /
+ * ⁠-10  5
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * @File    :   108.convert-sorted-array-to-binary-search-tree.java
+ * @Time    :   2020/04/24 20:40:57
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+class Solution108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return new TreeNode(nums[start]);
+        }
+        int mid = (start + end + 1) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = build(nums, start, mid - 1);
+        root.right = build(nums, mid + 1, end);
+        return root;
+    }
+}
+// @lc code=end
+
