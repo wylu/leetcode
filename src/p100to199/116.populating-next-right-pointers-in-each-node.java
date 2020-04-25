9ec7b8efc13fbc52d366c57ae02039694fd00ad9
wylu <158677478@qq.com>
2020-04-25 09:06:18 +0800

    Tree DFS

---
+++ src/p100to199/116.populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,125 @@
+package p100to199;
+
+import common.Node;
+
+/*
+ * @lc app=leetcode id=116 lang=java
+ *
+ * [116] Populating Next Right Pointers in Each Node
+ *
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+ *
+ * algorithms
+ * Medium (42.91%)
+ * Likes:    1656
+ * Dislikes: 136
+ * Total Accepted:    330.1K
+ * Total Submissions: 768.5K
+ * Testcase Example:  '[1,2,3,4,5,6,7]'
+ *
+ * You are given a perfect binary tree where all leaves are on the same level,
+ * and every parent has two children. The binary tree has the following
+ * definition:
+ * 
+ * 
+ * struct Node {
+ * ⁠ int val;
+ * ⁠ Node *left;
+ * ⁠ Node *right;
+ * ⁠ Node *next;
+ * }
+ * 
+ * 
+ * Populate each next pointer to point to its next right node. If there is no
+ * next right node, the next pointer should be set to NULL.
+ * 
+ * Initially, all next pointers are set to NULL.
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * You may only use constant extra space.
+ * Recursive approach is fine, you may assume implicit stack space does not
+ * count as extra space for this problem.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: root = [1,2,3,4,5,6,7]
+ * Output: [1,#,2,3,#,4,5,6,7,#]
+ * Explanation: Given the above perfect binary tree (Figure A), your function
+ * should populate each next pointer to point to its next right node, just like
+ * in Figure B. The serialized output is in level order as connected by the
+ * next pointers, with '#' signifying the end of each level.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the given tree is less than 4096.
+ * -1000 <= node.val <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+/**
+ * @File    :   116.populating-next-right-pointers-in-each-node.java
+ * @Time    :   2020/04/25 08:51:50
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+class Solution116 {
+    public Node connect(Node root) {
+        Node levelStart = root;
+        while (levelStart != null) {
+            Node cur = levelStart;
+            while (cur != null) {
+                if (cur.left != null) {
+                    cur.left.next = cur.right;
+                    if (cur.next != null) {
+                        cur.right.next = cur.next.left;
+                    }
+                }
+                cur = cur.next;
+            }
+            levelStart = levelStart.left;
+        }
+        return root;
+    }
+}
+// @lc code=end
+
