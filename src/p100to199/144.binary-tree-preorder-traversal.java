0ec5dda29e77e47afc6f7292129df4da7e5deb91
wylu <158677478@qq.com>
2020-04-25 10:31:39 +0800

    Tree Preorder-Traversal

---
+++ src/p100to199/144.binary-tree-preorder-traversal.java
@@ -0,0 +1,94 @@
+package p100to199;
+
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=144 lang=java
+ *
+ * [144] Binary Tree Preorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (54.46%)
+ * Likes:    1286
+ * Dislikes: 54
+ * Total Accepted:    458.7K
+ * Total Submissions: 841.9K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ * 
+ * Output: [1,2,3]
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * @File    :   144.binary-tree-preorder-traversal.java
+ * @Time    :   2020/04/25 10:14:35
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+class Solution144 {
+    /**
+     * 创建一个辅助栈：
+     * 1.访问当前根结点
+     * 2.将右子树压入栈中
+     * 3.将左子树压入栈中
+     *
+     * @param root 树的根结点
+     * @return 前序遍历结果
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            res.add(root.val);
+            if (root.right != null) {
+                stack.push(root.right);
+            }
+            if (root.left != null) {
+                stack.push(root.left);
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
