a412b96a6340dde0a4bd859dab675cd27800e065
wylu <158677478@qq.com>
2020-04-29 23:47:45 +0800

    BST inorder-traversal

---
+++ src/p500to599/538.convert-bst-to-greater-tree.java
@@ -0,0 +1,87 @@
+package p500to599;
+
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode id=538 lang=java
+ *
+ * [538] Convert BST to Greater Tree
+ *
+ * https://leetcode.com/problems/convert-bst-to-greater-tree/description/
+ *
+ * algorithms
+ * Easy (54.11%)
+ * Likes:    1740
+ * Dislikes: 111
+ * Total Accepted:    111.5K
+ * Total Submissions: 205.7K
+ * Testcase Example:  '[5,2,13]'
+ *
+ * Given a Binary Search Tree (BST), convert it to a Greater Tree such that
+ * every key of the original BST is changed to the original key plus sum of all
+ * keys greater than the original key in BST.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: The root of a Binary Search Tree like this:
+ * ⁠             5
+ * ⁠           /   \
+ * ⁠          2     13
+ * 
+ * Output: The root of a Greater Tree like this:
+ * ⁠            18
+ * ⁠           /   \
+ * ⁠         20     13
+ * 
+ * 
+ * Note: This question is the same as 1038:
+ * https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * @File    :   538.convert-bst-to-greater-tree.java
+ * @Time    :   2020/04/29 23:29:00
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+class Solution538 {
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+
+}
+// @lc code=end
+
