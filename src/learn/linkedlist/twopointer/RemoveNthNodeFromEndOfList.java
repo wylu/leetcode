5b1514ae235e57f2ec265393cc02965484a52340
wylu <158677478@qq.com>
2020-05-17 11:11:02 +0800

    Remove Nth Node From End of List

---
+++ src/learn/linkedlist/twopointer/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,29 @@
+package learn.linkedlist.twopointer;
+
+import common.ListNode;
+
+/**
+ * @File    :   RemoveNthNodeFromEndOfList.java
+ * @Time    :   2020/05/17 10:54:58
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode front = head, behind = head;
+        while (n-- > 0) front = front.next;
+        if (front == null) return behind.next;
+
+        while (front.next != null) {
+            front = front.next;
+            behind = behind.next;
+        }
+
+        ListNode node = behind.next;
+        behind.next = node.next;
+        return head;
+    }
+}
