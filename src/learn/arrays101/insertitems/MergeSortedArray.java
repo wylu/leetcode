06aa55b22c4f05cac6fc7a54f33ccdf53107244e
wylu <158677478@qq.com>
2020-05-18 22:17:09 +0800

    Merge Sorted Array

---
+++ src/learn/arrays101/insertitems/MergeSortedArray.java
@@ -0,0 +1,38 @@
+package learn.arrays101.insertitems;
+
+import java.util.Arrays;
+
+/**
+ * @File    :   MergeSortedArray.java
+ * @Time    :   2020/05/18 22:06:25
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (j >= 0) {
+            if (i < 0 || nums2[j] > nums1[i]) {
+                nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+        mergeSortedArray.merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+
+        nums1 = new int[]{4, 5, 6, 0, 0, 0};
+        nums2 = new int[]{1, 2, 3};
+        mergeSortedArray.merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
