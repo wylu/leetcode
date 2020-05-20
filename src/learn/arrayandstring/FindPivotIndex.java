d4a50311e6b9556b8d8fb5b120cf00282257da44
wylu <158677478@qq.com>
2020-05-20 20:53:41 +0800

    Find Pivot Index

---
+++ src/learn/arrayandstring/FindPivotIndex.java
@@ -0,0 +1,23 @@
+package learn.arrayandstring;
+
+/**
+ * @File    :   FindPivotIndex.java
+ * @Time    :   2020/05/20 20:36:00
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for (int e: nums) sum += e;
+
+        for (int i = 0, cur = 0; i < nums.length; i++) {
+            if (cur == sum - cur - nums[i]) return i;
+            cur += nums[i];
+        }
+        return -1;
+    }
+}
