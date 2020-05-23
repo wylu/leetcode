2b7877458d00d20ab2a444a323a37909803c081d
wylu <158677478@qq.com>
2020-05-23 11:57:25 +0800

    Longest Common Prefix

---
+++ src/learn/arrayandstring/string/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+package learn.arrayandstring.string;
+
+/**
+ * @File    :   LongestCommonPrefix.java
+ * @Time    :   2020/05/23 11:48:43
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if ("".equals(prefix)) return "";
+            }
+        }
+        return prefix;
+    }
+}
