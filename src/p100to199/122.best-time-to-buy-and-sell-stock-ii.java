ed1fa7313a0889271f2d5c0b59a82c34e87bd5cd
wylu <158677478@qq.com>
2020-06-18 23:25:14 +0800

    Best Time to Buy and Sell Stock II

---
+++ src/p100to199/122.best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,89 @@
+package p100to199;
+
+/*
+ * @lc app=leetcode id=122 lang=java
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+ *
+ * algorithms
+ * Easy (56.27%)
+ * Likes:    2314
+ * Dislikes: 1740
+ * Total Accepted:    596.7K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * Say you have an array prices for which the i^th element is the price of a
+ * given stock on day i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete as many
+ * transactions as you like (i.e., buy one and sell one share of the stock
+ * multiple times).
+ * 
+ * Note: You may not engage in multiple transactions at the same time (i.e.,
+ * you must sell the stock before you buy again).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit
+ * = 5-1 = 4.
+ * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 =
+ * 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit
+ * = 5-1 = 4.
+ * Note that you cannot buy on day 1, buy on day 2 and sell them later, as you
+ * are
+ * engaging multiple transactions at the same time. You must sell before buying
+ * again.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= prices.length <= 3 * 10 ^ 4
+ * 0 <= prices[i] <= 10 ^ 4
+ * 
+ * 
+ */
+
+/**
+ * @File    :   122.best-time-to-buy-and-sell-stock-ii.java
+ * @Time    :   2020/06/18 23:21:23
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+// @lc code=start
+class Solution122 {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                res += prices[i] - prices[i - 1];
+        }
+        return res;
+    }
+}
+// @lc code=end
