2f7107fcdf1eac5a1c6c0974924c1576e1f1a974
wylu <158677478@qq.com>
2020-06-28 22:58:05 +0800

    Set Matrix Zeroes

---
+++ src/p1to99/73.set-matrix-zeroes.java
@@ -0,0 +1,117 @@
+package p1to99;
+
+/*
+ * @lc app=leetcode id=73 lang=java
+ *
+ * [73] Set Matrix Zeroes
+ *
+ * https://leetcode.com/problems/set-matrix-zeroes/description/
+ *
+ * algorithms
+ * Medium (42.71%)
+ * Likes:    2038
+ * Dislikes: 297
+ * Total Accepted:    311.9K
+ * Total Submissions: 728.4K
+ * Testcase Example:  '[[1,1,1],[1,0,1],[1,1,1]]'
+ *
+ * Given a m x n matrix, if an element is 0, set its entire row and column to
+ * 0. Do it in-place.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 
+ * [
+ *   [1,1,1],
+ *   [1,0,1],
+ *   [1,1,1]
+ * ]
+ * Output: 
+ * [
+ *   [1,0,1],
+ *   [0,0,0],
+ *   [1,0,1]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 
+ * [
+ *   [0,1,2,0],
+ *   [3,4,5,2],
+ *   [1,3,1,5]
+ * ]
+ * Output: 
+ * [
+ *   [0,0,0,0],
+ *   [0,4,5,0],
+ *   [0,3,1,0]
+ * ]
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * A straight forward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best
+ * solution.
+ * Could you devise a constant space solution?
+ * 
+ * 
+ */
+
+/**
+ * @File    :   73.set-matrix-zeroes.java
+ * @Time    :   2020/06/28 22:28:35
+ * @Author  :   wylu
+ * @Version :   1.0
+ * @Contact :   15wylu@gmail.com
+ * @License :   Copyright © 2020, wylu-CHINA-SHENZHEN. All rights reserved.
+ * @Desc    :
+ */
+// @lc code=start
+class Solution73 {
+    public void setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return;
+
+        boolean firstCol = false;
+        int rows = matrix.length, cols = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstCol = true;
+            }
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (firstCol) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+// @lc code=end
